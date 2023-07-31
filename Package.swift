6d5b7dd3e5ed42e1ece352844a03c9c3899b896c
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2023-07-31 15:29:15 +0100

    docs: adding README files to subdirectories and including in target exclusions

--- Package.swift
+++ Package.swift
@@ -12,40 +12,38 @@ let package = Package(
         .library(name: "GAnalytics", targets: ["GAnalytics"])
     ],
     dependencies: [
-      .package(
-        url: "https://github.com/firebase/firebase-ios-sdk.git",
-        .upToNextMajor(from: "9.4.1")
-      ),
-      .package(
-        url: "https://github.com/alphagov/di-mobile-ios-networking.git",
-        .upToNextMajor(from: "1.0.0")
-      )
+        .package(
+            url: "https://github.com/firebase/firebase-ios-sdk.git",
+            .upToNextMajor(from: "9.4.1")
+        ),
+        .package(
+            url: "https://github.com/alphagov/di-mobile-ios-networking.git",
+            .upToNextMajor(from: "1.0.0")
+        )
     ],
     targets: [
         .target(name: "Analytics",
+                exclude: ["README.md"],
                 swiftSettings: [
                     .define("DEBUG", .when(configuration: .debug))
                 ]),
-        .testTarget(name: "AnalyticsTests", dependencies: ["Analytics"]),
+        .testTarget(name: "AnalyticsTests",
+                    dependencies: ["Analytics"]),
         
-        .target(
-            name: "Logging",
-            dependencies: ["Analytics", .product(name: "Networking",
-                                                 package: "di-mobile-ios-networking")],
-            swiftSettings: [
-                .define("DEBUG", .when(configuration: .debug))
-            ]),
-        .testTarget(name: "LoggingTests", dependencies: [
-            "Logging",
-            .product(name: "MockNetworking", package: "di-mobile-ios-networking")
-        ]),
+        .target(name: "Logging",
+                dependencies: ["Analytics", .product(name: "Networking", package: "di-mobile-ios-networking")],
+                exclude: ["README.md"],
+                swiftSettings: [
+                    .define("DEBUG", .when(configuration: .debug))
+                ]),
+        .testTarget(name: "LoggingTests",
+                    dependencies: ["Logging", .product(name: "MockNetworking", package: "di-mobile-ios-networking")]),
         
-        .target(name: "GAnalytics", dependencies: [
-            "Analytics",
-            .product(name: "FirebaseAnalyticsWithoutAdIdSupport", package: "firebase-ios-sdk"),
-            .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk")
-        ], swiftSettings: [
-            .define("DEBUG", .when(configuration: .debug))
-        ])
+        .target(name: "GAnalytics",
+                dependencies: ["Analytics", .product(name: "FirebaseAnalyticsWithoutAdIdSupport", package: "firebase-ios-sdk"), .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk")],
+                exclude: ["README.md"],
+                swiftSettings: [
+                    .define("DEBUG", .when(configuration: .debug))
+                ])
     ]
 )
