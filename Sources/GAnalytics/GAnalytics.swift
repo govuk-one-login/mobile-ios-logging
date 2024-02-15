1544e582456f59f315da782880ad4582f00cf0b0
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2024-02-15 15:20:26 +0000

    fix: adding screenID

--- Sources/GAnalytics/GAnalytics.swift
+++ Sources/GAnalytics/GAnalytics.swift
@@ -41,6 +41,7 @@ public class GAnalytics: AnalyticsService {
         
         parameters[AnalyticsParameterScreenName] = screen.title
         parameters[AnalyticsParameterScreenClass] = screen.type
+        parameters["screen_id"] = screen.screenID
         
         Analytics.logEvent(AnalyticsEventScreenView,
                            parameters: parameters)
