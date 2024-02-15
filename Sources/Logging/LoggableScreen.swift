0c2cf636e9c98ae64c76388177b7ca16ced1e836
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2024-02-15 14:19:24 +0000

    fix: refactor of loggable screen

--- Sources/Logging/LoggableScreen.swift
+++ Sources/Logging/LoggableScreen.swift
@@ -5,17 +5,3 @@ public protocol LoggableScreen {
     var title: String { get }
     var type: String { get }
 }
-
-extension LoggableScreen where Self: RawRepresentable,
-                                Self.RawValue == String {
-    
-    /// Protocol method returning the string value from a Type's `name` property which conforms to this protocol.
-    public var title: String {
-        rawValue
-    }
-    
-    /// Protocol method returning the string value from a Type's `type` property which conforms to this protocol.
-    public var type: String {
-        rawValue
-    }
-}
