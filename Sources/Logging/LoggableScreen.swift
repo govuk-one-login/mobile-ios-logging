3cd8c8387c66dc3a2254f9ac40af374a7744af92
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2024-02-15 16:57:25 +0000

    fix: readding extension

--- Sources/Logging/LoggableScreen.swift
+++ Sources/Logging/LoggableScreen.swift
@@ -4,3 +4,12 @@
 public protocol LoggableScreen {
     var name: String { get }
 }
+
+extension LoggableScreen where Self: RawRepresentable,
+                                Self.RawValue == String {
+    
+    /// Protocol method returning the string value from a Type's `name` property which conforms to this protocol.
+    public var name: String {
+        rawValue
+    }
+}
