670da2cac82e43b5989bc71c9ab0b732782123c0
Shukri Ahmed <shukri.ahmed@digital.cabinet-office.gov.uk>
2024-03-06 11:45:36 +0000

    chore: added defaultLocalization to Package file

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Logging",
+    defaultLocalization: "en",
     platforms: [.iOS(.v13)],
     products: [
         .library(name: "Logging", targets: ["Logging"]),
