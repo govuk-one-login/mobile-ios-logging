ba9a80e6cdcaae699122c90ff3bc105accf444f9
Robin Phillips <robin.phillips1@digital.cabinet-office.gov.uk>
2025-03-06 18:25:27 +0000

    feat: added AuthorizedHTTPLogger for making posting logged events to scoped services

---
+++ Sources/HTTPLogging/AuthorizedHTTPLogger.swift
@@ -0,0 +1,57 @@
+import Foundation
+import Logging
+import Networking
+
+/// AuthorizedHTTPLogger
+///
+/// A struct for sending HTTP requests to endpoints secured with scoped access tokens.
+/// This logger can be used to log user/journey specific insights for app metrics and performance.
+public struct AuthorizedHTTPLogger {
+    /// `NetworkClient` from the Networking package dependency to handle HTTP networking
+    let networkClient: NetworkClient
+    /// `URL` address for sending HTTP requests
+    let loggingURL: URL
+    /// Scope for service access token
+    let scope: String
+    /// callback to handle possible errors resulting from `NetworkClient`'s `makeRequest` method
+    let handleError: ((Error) -> Void)?
+
+    /// Initialiser for class with default methods for `networkClient` and `handleError` parameters
+    init(
+        url: URL,
+        networkClient: NetworkClient,
+        scope: String,
+        handleError: ((Error) -> Void)? = nil
+    ) {
+        loggingURL = url
+        self.scope = scope
+        self.networkClient = networkClient
+        self.handleError = handleError
+    }
+
+    /// Sends HTTP POST request to designated URL, handling errors received back from `NetworkClient`'s `makeAuthorizedRequest` method
+    /// - Parameters:
+    ///     - event: the encodable object to be logged in the request body as JSON
+    public func logEvent(requestBody: any Encodable) {
+        guard let jsonData = try? JSONEncoder().encode(requestBody) else {
+            assertionFailure("Failed to decode object")
+            return
+        }
+
+        Task {
+            var request = URLRequest(url: loggingURL)
+            request.httpMethod = "POST"
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpBody = jsonData
+
+            do {
+                _ = try await networkClient.makeAuthorizedRequest(
+                    scope: scope,
+                    request: request
+                )
+            } catch {
+                handleError?(error)
+            }
+        }
+    }
+}
---
+++ Tests/HTTPLoggingTests/MockEvent.swift
@@ -0,0 +1,135 @@
+@testable import HTTPLogging
+import Logging
+import MockNetworking
+@testable import Networking
+import XCTest
+
+struct MockAuthorizedRequestBody: Codable {
+    struct MockSubType: Codable {
+        let specialInfo: String
+        
+        init(specialInfo: String = "special info") {
+            self.specialInfo = specialInfo
+        }
+    }
+    
+    let name: String
+    let timestamp: Int
+    
+    let subType: MockSubType
+    
+
+    init(
+        name: String = "mock request name",
+        timestamp: Int = 123456789012,
+        subType: MockSubType = MockSubType()
+    ) {
+        self.name = name
+        self.timestamp = timestamp
+        self.subType = subType
+    }
+}
+
+struct MockTokenHolder: AuthorizationProvider {
+    func fetchToken(withScope scope: String) async throws -> String {
+        "mockToken"
+    }
+}
+
+final class AuthorizedHTTPLoggerTests: XCTestCase {
+    private var sut: AuthorizedHTTPLogger!
+    private var client: NetworkClient!
+    private var configuration: URLSessionConfiguration!
+    
+    override func setUp() {
+        super.setUp()
+        configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [MockURLProtocol.self]
+        let mockTokenHolder = MockTokenHolder()
+        client = NetworkClient(configuration: configuration)
+        client.authorizationProvider = mockTokenHolder
+        
+        sut = AuthorizedHTTPLogger(
+            url: URL(string: "https://example.com/dev")!,
+            networkClient: client,
+            scope: "this.is.scoped"
+        )
+    }
+    
+    override func tearDown() {
+        sut = nil
+        client = nil
+        MockURLProtocol.clear()
+        configuration = nil
+        super.tearDown()
+    }
+}
+
+extension AuthorizedHTTPLoggerTests {
+    func test_successfulTXMAEventLog() throws {
+        // GIVEN network client returns 200
+        MockURLProtocol.handler = {
+            (Data(), HTTPURLResponse(statusCode: 200))
+        }
+        
+        // WHEN an event is logged
+        let requestBody = MockAuthorizedRequestBody()
+        sut.logEvent(requestBody: requestBody)
+        
+        wait(for: [
+            XCTNSPredicateExpectation(
+                predicate: .init(
+                    block: { _, _ in
+                        MockURLProtocol.requests.count == 1
+                    }
+                ),
+                object: nil
+            )
+        ], timeout: 3)
+        
+        // THEN the request succeeds
+        let request = try XCTUnwrap(MockURLProtocol.requests.first)
+        XCTAssertEqual(request.httpMethod, "POST")
+        XCTAssertEqual(request.url?.scheme, "https")
+        XCTAssertEqual(request.url?.host, "example.com")
+        XCTAssertEqual(request.url?.path, "/dev")
+
+        let httpData = try XCTUnwrap(request.httpBodyData())
+        let decoder = JSONDecoder()
+        let httpBody = try decoder.decode(MockAuthorizedRequestBody.self, from: httpData)
+
+        XCTAssertEqual(httpBody.name, "mock request name")
+        XCTAssertEqual(httpBody.timestamp, 123456789012)
+        XCTAssertEqual(httpBody.subType.specialInfo, "special info")
+    }
+    
+    func test_TXMAEventLog_throwsError() throws {
+        var error: ServerError?
+        
+        sut = AuthorizedHTTPLogger(
+            url: URL(string: "https://example.com/dev")!,
+            networkClient: client,
+            scope: "this.is.scoped",
+            handleError: { receivedError in
+                error = receivedError as? ServerError
+            }
+        )
+        
+        // GIVEN network client returns 401 error
+        MockURLProtocol.handler = {
+            (Data(), HTTPURLResponse(statusCode: 401))
+        }
+        
+        // WHEN a TXMA event is logged
+        let requestBody = MockAuthorizedRequestBody()
+        sut.logEvent(requestBody: requestBody)
+        wait(for: [
+            XCTNSPredicateExpectation(predicate: .init(block: { _, _ in
+                MockURLProtocol.requests.count == 1
+            }), object: nil)
+        ], timeout: 3)
+        
+         
+        XCTAssertEqual(error?.errorCode, 401)
+    }
+}
