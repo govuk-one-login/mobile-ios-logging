35404e8468cf85e80ea7e16c42f2c58b1375dfbf
Robin Phillips <104073334+robinphillips@users.noreply.github.com>
2025-03-07 16:46:17 +0000

    feat: added AuthorizedHTTPLogger for posting logged events to scoped endpoints (#73)
    * feat: added AuthorizedHTTPLogger for making posting logged events to scoped services
    
    * feat: made init public
    
    * chore: renaming file
    
    * chore: renaming message in assertion failures
    
    * chore: removing logging import
    
    * chore: changing order of properties
    
    * feat: added task cancellation and async version of log event
    
    * feat: adjusted task cancellation
    
    * chore: tidying up
    
    * feat: added line to HTTPLogging documentation
    
    * chore: minor tweaks

---
+++ Sources/HTTPLogging/AuthorizedHTTPLogger.swift
@@ -0,0 +1,70 @@
+import Foundation
+import Networking
+
+/// AuthorizedHTTPLogger
+///
+/// A struct for sending HTTP requests to endpoints secured with scoped access tokens.
+/// This logger can be used to log user/journey specific insights for app metrics and performance.
+public struct AuthorizedHTTPLogger {
+    /// `URL` address for sending HTTP requests
+    let loggingURL: URL
+    /// `NetworkClient` from the Networking package dependency to handle HTTP networking
+    let networkClient: NetworkClient
+    /// Scope for service access token
+    let scope: String
+    /// callback to handle possible errors resulting from `NetworkClient`'s `makeRequest` method
+    let handleError: ((Error) -> Void)?
+
+    /// Initialiser for class with default methods for `networkClient` and `handleError` parameters
+    public init(
+        url: URL,
+        networkClient: NetworkClient,
+        scope: String,
+        handleError: ((Error) -> Void)? = nil
+    ) {
+        loggingURL = url
+        self.scope = scope
+        self.networkClient = networkClient
+        self.handleError = handleError
+    }
+
+    /// Sends HTTP POST request to designated URL, handling errors received back from `NetworkClient`'s `makeAuthorizedRequest` method
+    /// - Parameters:
+    ///     - event: the encodable object to be logged in the request body as JSON
+    @discardableResult
+    public func logEvent(requestBody: any Encodable) -> Task<Void, Never>? {
+        guard let jsonData = try? JSONEncoder().encode(requestBody) else {
+            assertionFailure("Failed to encode object")
+            return nil
+        }
+
+        return Task {
+            await createAndMakeRequest(data: jsonData)
+        }
+    }
+    
+    public func logEvent(requestBody: any Encodable) async {
+        guard let jsonData = try? JSONEncoder().encode(requestBody) else {
+            assertionFailure("Failed to encode object")
+            return
+        }
+        
+        await createAndMakeRequest(data: jsonData)
+    }
+    
+    private func createAndMakeRequest(data: Data) async {
+        var request = URLRequest(url: loggingURL)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = data
+        
+        do {
+            _ = try await networkClient.makeAuthorizedRequest(
+                scope: scope,
+                request: request
+            )
+        } catch {
+            handleError?(error)
+        }
+    }
+}
---
+++ Sources/HTTPLogging/HTTPLoggerError.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+enum HTTPLoggerError: Error {
+    case couldNotSerializeData
+}
