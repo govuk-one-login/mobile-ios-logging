62bced722bd2d4f5e1dc4873c8e7bee4b0fcd89e
Robin Phillips <robin.phillips1@digital.cabinet-office.gov.uk>
2026-01-26 14:37:02 +0000

    fix: add back old methods to analytics service v3

--- Sources/Logging/AnalyticsServiceV3.swift
+++ Sources/Logging/AnalyticsServiceV3.swift
@@ -3,7 +3,6 @@ import Foundation
 /// AnalyticsServiceV3
 ///
 /// A protocol for Types to log analytics to a third-party analytics service.
-@available(*, deprecated, renamed: "AnalyticsService", message: "This will be renamed - please use typealias AnalyticsService instead")
 public protocol AnalyticsServiceV3: LoggingService {
     var performanceLogger: PerformanceLogger { get }
     var analyticsPreferenceStore: AnalyticsPreferenceStore { get }
@@ -11,11 +10,25 @@ public protocol AnalyticsServiceV3: LoggingService {
     var additionalParameters: [String: Any] { get set }
     func addingAdditionalParameters(_ additionalParameters: [String: Any]) -> Self
     
+    @available(*, deprecated, renamed: "trackScreen", message: "Please use LoggableScreenV2")
+    func trackScreen(_ screen: LoggableScreen)
+    
+    @available(*, deprecated, renamed: "trackScreen", message: "Please use LoggableScreenV2")
+    func trackScreen(_ screen: LoggableScreen, parameters: [String: Any])
+    
     func trackScreen(_ screen: any LoggableScreenV2, parameters: [String: Any])
     
     func logCrash(_ crash: NSError)
     func logCrash(_ crash: Error)
 }
+
+extension AnalyticsServiceV3 {
+    /// Protocol method for screen tracking, calling the conforming type's method for adding screen tracking parameters.
+    public func trackScreen(_ screen: LoggableScreen) {
+        trackScreen(screen, parameters: [:])
+    }
+}
+
 
 public protocol PerformanceLogger {
     var isEnabled: Bool { get }
