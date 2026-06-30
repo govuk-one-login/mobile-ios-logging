bd4dfcc1958e8dda7286156ac4816d6aff8a3401
Kotsalidou, Chionia <chionia.kotsalidou@digital.cabinet-office.gov.uk>
2026-06-30 16:55:19 +0100

    chore: clean up

--- Sources/HTTPLogging/HTTPLoggingNetworkClient.swift
+++ Sources/HTTPLogging/HTTPLoggingNetworkClient.swift
@@ -3,15 +3,4 @@ import Networking
 
 public protocol HTTPLoggingNetworkClient {
     func request(_ request: URLRequest) -> RequestBuilder
-    func makeRequest(_ request: NetworkRequest) async throws -> Data
-    
-    // TODO: DCMAW-20368 Remove these
-    @available(*, deprecated, message: "use .request().execute() instead")
-    func makeRequest(_ request: URLRequest) async throws -> Data
-    
-    @available(*, deprecated, message: "use .request().withAuthentication().execute() instead")
-    func makeAuthorizedRequest(
-        scope: String,
-        request: URLRequest
-    ) async throws -> Data
 }
