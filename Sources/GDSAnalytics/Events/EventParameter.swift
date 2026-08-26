c43eda949c3f3a9ecb2bab8aadd52873cbf15949
Kriss Bennett <kriss.bennett@digital.cabinet-office.gov.uk>
2026-08-26 14:13:27 +0100

    feat: Add systemEvent

--- Sources/GDSAnalytics/Events/EventParameter.swift
+++ Sources/GDSAnalytics/Events/EventParameter.swift
@@ -4,4 +4,8 @@ public enum EventParameter: String {
     case response
     case linkDomain = "link_domain"
     case external
+    case reason
+    case isError = "is_error"
+    case savedDocType = "saved_doc_type"
+    case firebaseScreen = "firebase_screen"
 }
